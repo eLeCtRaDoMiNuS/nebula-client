6f01933e7759a384ee9229f3de5a5bdefc49a1b6
llameiq <77410796+eLeCtRaDoMiNuS@users.noreply.github.com>
2022-09-12 10:50:17 -0700

    Create customvapenpm.lua

---
+++ assets/customvapenpm.lua
@@ -0,0 +1,7 @@
+--[[
+npms custom config
+bro pls dont leak this i beg u
+btw scepty is a noob
+becouse of that ur gonna leak that arent u
+well too bad im not gonna make it a loadstring or make it public
+]]--
